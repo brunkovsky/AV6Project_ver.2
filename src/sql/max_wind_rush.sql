b1bac4d4b94c1baf16fa84ccdf22023e3d07c855
root <root@mail.com>
2016-04-13 17:50:41 +0000

    SQL-query added again

--- src/sql/max_wind_rush.sql
+++ src/sql/max_wind_rush.sql
@@ -1,3 +1,10 @@
-SELECT date, MAX(wind_rush)
+# SELECT date, MAX(wind_rush)
+# FROM table_av6
+# WHERE date BETWEEN '2016-02-01' AND '2016-02-29';
+
+SELECT CAST(date AS DATE) AS only_date
 FROM table_av6
-WHERE date BETWEEN '2016-02-01' AND '2016-02-29';
+WHERE wind_rush = (SELECT MAX(wind_rush)
+       FROM table_av6
+       WHERE date BETWEEN '2015-01-01' AND '2016-02-29') AND
+      (date BETWEEN '2015-01-01' AND '2016-02-29')
