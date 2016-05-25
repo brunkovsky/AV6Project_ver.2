e4c7143b286e440785eb1e37003f83b34ec6d2bb
root <root@mail.com>
2016-05-25 06:43:01 +0000

    min_temp_air, max_temp_air fixed

--- src/sql/max_temp_air.sql
+++ src/sql/max_temp_air.sql
@@ -1,1 +1,5 @@
-SELECT MAX(max_temp_air) FROM table_av6_extended;
+SELECT date, max_temp_air
+FROM table_av6_extended
+WHERE max_temp_air=(SELECT MAX(max_temp_air)
+                    FROM table_av6_extended
+                    WHERE date BETWEEN '2015-01-01' AND '2015-02-01');
--- src/sql/min_temp_air.sql
+++ src/sql/min_temp_air.sql
@@ -1,1 +1,5 @@
-SELECT MIN(min_temp_air) FROM table_av6_extended;
+SELECT date, min_temp_air
+FROM table_av6_extended
+WHERE min_temp_air=(SELECT MIN(min_temp_air)
+                    FROM table_av6_extended
+                    WHERE date BETWEEN '2015-01-01' AND '2015-02-01');
